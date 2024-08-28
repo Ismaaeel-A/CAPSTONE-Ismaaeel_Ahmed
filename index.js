6136619df62a0de6916e8855e947ae0581d3e0d1
Ismaaeel-A <ismaaeelahmed42@gmail.com>
2024-08-28 09:46:56 +0200

    created config file and added to index file

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-/*  */
+import express from 'express'
+import { configDotenv } from 'dotenv'
+configDotenv()
+
+//CREATE AN EXPRESS APP
+const app = express()
+const port = +process.env.PORT || 4000
+
+//MIDDLEWARE
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "Authorization");
+    next()
+}) 
+
+//ENDPOINT
+app.get('^/$|/EOMP', (req, res) => {
+    res.status(200).sendFile(path.resolve('./static/html/index.html'))
+})
+
+app.get('*', (req, res) => {
+    res.json({
+        status: 404,
+        err: 'Resource not found.'
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+})
