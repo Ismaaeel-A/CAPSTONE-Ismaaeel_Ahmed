bba51a2a396780a53b07c9fd3b55466322e87e73
Ismaaeel-A <ismaaeelahmed42@gmail.com>
2024-08-30 11:33:15 +0200

    basic store testing

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,73 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+import { toast } from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
+
+const apiURL = 'https://capstone-ismaaeel-ahmed.onrender.com/'
 
 export default createStore({
   state: {
+    users: null,
+    user: null,
+    products: null,
+    product: null
   },
+
   getters: {
   },
+
   mutations: {
+    setUsers(state, value) {
+      state.users = value
+    },
+    setUser(state, value) {
+      state.user = value
+    },
+    setProducts(state, value) {
+      state.products = value
+    },
+    setProduct(state, value) {
+      state.product = value
+    },
   },
+
   actions: {
+    async fetchUsers(context) {
+      try {
+        const { results, msg } = await (await axios.get(`${apiURL}users`)).data
+        if (results) {
+          context.commit('setUsers', results)
+        } else {
+          toast.error(`${msg}`, {
+            autoClose: 3000
+          })
+        }
+
+      } catch (e) {
+        toast.error(`${e.msg}`, {
+          autoClose: 3000
+        })
+      }
+    },
+
+    async fetchProducts(context) {
+      try {
+        const { results, msg } = await (await axios.get(`${apiURL}products`)).data
+        if (results) {
+          context.commit('setProducts', results)
+        } else {
+          toast.error(`${msg}`, {
+            autoClose: 3000
+          })
+        }
+
+      } catch (e) {
+        toast.error(`${e.msg}`, {
+          autoClose: 3000
+        })
+      }
+    },
+
   },
   modules: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="container-fluid"></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+  },
+/*   computed: {
+    users() {
+      return this.$store.state.users;
+    },
+    products() {
+      return this.$store.state.products;
+    }
+  },
+  mounted() {
+    this.$store.dispatch("fetchUsers");
+    this.$store.dispatch("fetchProducts");
+  }, */
+};
 </script>
+
+<style></style>
