12b1e3c46c39dea8e3d1e66561284b15baaf1a53
Ismaaeel-A <ismaaeelahmed42@gmail.com>
2024-09-02 21:59:59 +0200

    showroom edit + card comp

---
+++ src/components/DisplayCard.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="card g-5">
+      <div class="card-header">
+          <slot name="cardHeader"></slot>
+      </div>
+  
+      <div class="card-body">
+          <slot name="cardBody"></slot>
+      </div>
+    </div>
+  </template>
+
+<style scoped>
+  .card{
+    width: 38rem;
+    background: #2a2a2a;
+    text-align: center;
+    border: none;
+    
+    &:hover{
+      & .card-body , .card-header{
+        transition: background 2s;
+        background: #818181;
+      } 
+    }
+  }
+
+  .card-body{
+    border-top: 4px solid #e9e9e9;
+    border-bottom: 1px solid #e9e9e9;
+  }
+  
+</style>
--- src/views/ShowroomView.vue
+++ src/views/ShowroomView.vue
@@ -2,42 +2,66 @@
   <div class="container-fluid section">
     <div class="row">
       <div class="col-md-4">
-        <img
-          src="https://ismaaeel-a.github.io/allimages/Images/cars1.jpg"
-          alt="l"
-        />
+        <img src="https://ismaaeel-a.github.io/allimages/Images/cars1.jpg" alt="l" />
         <h4 class="centered">Lamborghini</h4>
       </div>
       <div class="col-md-4">
-        <img
-          src="https://ismaaeel-a.github.io/allimages/Images/cars2.jpg"
-          alt="p"
-        />
+        <img src="https://ismaaeel-a.github.io/allimages/Images/cars2.jpg" alt="p" />
         <h4 class="centered">Ferrari</h4>
       </div>
       <div class="col-md-4">
-        <img
-          src="https://ismaaeel-a.github.io/allimages/Images/cars1.jpg"
-          alt="f"
-        />
+        <img src="https://ismaaeel-a.github.io/allimages/Images/cars1.jpg" alt="f" />
         <h4 class="centered">Pagani</h4>
       </div>
     </div>
 
-    <main>
+    <main class="container">
       <h2 class="text-center mt-4">The Showroom</h2>
 
-      <div class="d-flex justify-content-center">
+      <div class="row justify-content-center">
         <input type="search" class="mt-2" />
       </div>
+
+      <div class="row justify-content-center" v-if="products">
+        <DisplayCard v-for="(products, productID) in products" :key="productID">
+          <template #cardHeader>
+            <img :src="products.prodImg1" alt="">
+          </template>
+
+          <template #cardBody>
+            <h6>{{ products.prodName }}</h6>
+          </template>
+
+        </DisplayCard>
+      </div>
+
     </main>
   </div>
 </template>
+
+<script>
+import DisplayCard from '@/components/DisplayCard.vue';
+
+export default {
+  components: {
+    DisplayCard,
+  },
+   computed: {
+    products() {
+      return this.$store.state.products;
+    }
+  },
+  mounted() {
+    this.$store.dispatch("fetchProducts");
+  }, 
+};
+</script>
 
 <style scoped>
 * {
   color: #e9e9e9;
 }
+
 .col-md-4 {
   display: flex;
   justify-content: center;
@@ -50,15 +74,17 @@
   color: #e9e9e9;
 
   & img {
+    border-radius: 0.8rem;
     max-width: 100%;
     transition: transform 0.3s;
-  }
 
-  &:hover {
-    & img {
+    &:hover {
       transform: scale(0.98);
     }
-    & h4:hover:after {
+  }
+}
+
+ h4:hover:after {
       width: 100%;
       left: 0;
     }
@@ -75,8 +101,6 @@
       transition: width 0.3s ease 0s, left 0.3s ease 0s;
       width: 0;
     }
-  }
-}
 
 .centered {
   position: absolute;
@@ -85,7 +109,7 @@
   transform: translate(-50%, -50%);
 }
 
-h2{
+h2 {
   text-decoration: underline;
 }
 
@@ -97,5 +121,13 @@ input {
   justify-content: center;
   background-color: #181818;
   padding-inline: 1rem;
+
+  &:focus {
+    outline: none;
+  }
 }
+
+img{
+    width: 100%;
+  }
 </style>
